73a2689665718c30b9f50b732c8183881a4f4e00
awataki <c.sharp.wind@gmail.com>
2018-05-09 12:02:09 +0900

    add script

---
+++ script/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output DP1 --off --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --off --output DP2 --off
+feh --bg-fill /usr/share/pixmaps/yayoi.PNG
+sh ~/.config/script/bar.sh
