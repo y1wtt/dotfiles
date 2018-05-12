414645c2a67b5c3d4061a90b8f691f1a3a46cd61
awataki <c.sharp.wind@gmail.com>
2018-05-12 16:30:46 +0900

    temp

--- fish/config.fish
+++ fish/config.fish
@@ -5,5 +5,4 @@ set -x XMODIFIERS @im=fcitx
 set -x TERM xterm-color
 set -x GOPATH "$HOME/.go/"
 set -x TERMINAL sakura
-set -x PATH $PATH /opt/firefox /opt/android-studio/bin /opt/idea-IU-173.4674.33/bin ~/.linuxbrew/bin/ /opt/clion-2018.1.1/bin /opt/GoLand-2018.1.1/bin
-status --is-login; and status --is-interactive; and exec byobu-launcher
+set -x PATH $PATH /opt/firefox ~/.linuxbrew/bin/
