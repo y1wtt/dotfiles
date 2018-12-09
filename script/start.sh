1a6412ea48ba598b76980f010e8597756406e650
awataki <awataki@yayoi.moe>
2018-12-09 22:34:20 +0900

    MultiBattery

--- script/start.sh
+++ script/start.sh
@@ -3,6 +3,7 @@ sh ~/.config/script/xrandr.sh &
 wait
 sh ~/.config/script/feh.sh
 sh ~/.config/script/bar.sh
-sudo nm-applet
+sudo killall nm-applet
+nm-applet
 sleep 30
 google-drive-ocamlfuse ./Google-Drive/
