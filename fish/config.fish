8fc44f099f8b5d36bbda76f4bd675348595b2078
awataki <awataki@yayoi.moe>
2018-12-15 01:50:08 +0900

    update environmet

--- fish/config.fish
+++ fish/config.fish
@@ -5,4 +5,5 @@ set -x XMODIFIERS @im=fcitx
 set -x TERM xterm-color
 set -x GOPATH "$HOME/.go/"
 set -x TERMINAL sakura
-set -x PATH $PATH /opt/firefox ~/.linuxbrew/bin/
+set -x PATH $PATH /opt/firefox ~/.linuxbrew/bin/ /opt/flutter/bin/ /opt/Sdk/platform-tools
+set -x ANDROID_HOME /opt/Sdk
--- script/start.sh
+++ script/start.sh
@@ -5,5 +5,3 @@ sh ~/.config/script/feh.sh
 sh ~/.config/script/bar.sh
 sudo killall nm-applet
 nm-applet
-sleep 30
-google-drive-ocamlfuse ./Google-Drive/
