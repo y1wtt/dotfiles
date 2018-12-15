83607e7ffb23ed66cd5f0bc5620128ee17122f24
awataki <awataki@yayoi.moe>
2018-12-15 15:30:01 +0900

    enable HiDPi

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,4 @@ set -x GOPATH "$HOME/.go/"
 set -x TERMINAL sakura
 set -x PATH $PATH /opt/firefox ~/.linuxbrew/bin/ /opt/flutter/bin/ /opt/Sdk/platform-tools
 set -x ANDROID_HOME /opt/Sdk
+set -x WINIT_HIDPI_FACTOR 1.0
