5e814de222a90529738e7ad84c2e5ea47fd7d4f7
awataki <awataki1206@gmail.com>
2019-07-16 15:48:10 +0900

    remove unused scripts

--- fish/functions/n.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 0
-function n --description 'alias n=nvim'
-	nvim  $argv;
-end
--- fish/functions/v.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 0
-function v --description 'alias v=nvim'
-	nvim  $argv;
-end
